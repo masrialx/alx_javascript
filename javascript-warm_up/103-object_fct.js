16485a8e93f53c0be08a36ffbdc7818bc6d82305
Masri Alemu <masrialemu@gmail.com>
2023-09-04 20:34:39 +0300

    v8.1

--- javascript-warm_up/103-object_fct.js
+++ javascript-warm_up/103-object_fct.js
@@ -2,10 +2,12 @@
 
 const myObject = {
     type: 'object',
-    value: 12,
-    incr: function () {
-      this.value++;
-    }
+    value: 12
+  };
+  
+  // Define the incr function
+  myObject.incr = function () {
+    this.value++;
   };
   
   console.log(myObject);
