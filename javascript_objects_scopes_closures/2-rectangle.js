6c897ed1df097df8cf0e1146aa6ecc5dfd4a874a
Masri Alemu <masrialemu@gmail.com>
2023-09-04 22:34:46 +0300

    v2.0

---
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,14 @@
+class Rectangle {
+    constructor(w, h) {
+      if (w <= 0 || h <= 0) {
+        // If either width or height is not a positive integer, create an empty object
+        return {};
+      }
+  
+      this.width = w;
+      this.height = h;
+    }
+  }
+  
+  module.exports = Rectangle;
+  
