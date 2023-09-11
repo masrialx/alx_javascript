5f7abdda8f626ab86b354a9e60fdb2d76f66b83a
Masri Alemu <masrialemu@gmail.com>
2023-09-11 19:55:37 +0300

    v2.4

--- javascript-network/2-starwars_count.js
+++ javascript-network/2-starwars_count.js
@@ -9,22 +9,24 @@ const apiUrl = process.argv[2];
 const characterId = 18;
 
 request.get(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error('Error:', error.message);
-  } else if (response.statusCode !== 200) {
-    console.error('Error:', `HTTP Status Code: ${response.statusCode}`);
-  } else {
-    try {
-      const filmsData = JSON.parse(body).results;
-      const numberOfFilms = filmsData.reduce((count, film) => {
-        if (film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)) {
-          return count + 1;
-        }
-        return count;
-      }, 0);
-      console.log(numberOfFilms);
-    } catch (parseError) {
-      console.error('Error:', 'Failed to parse response data');
+    if (error) {
+      console.error('Error:', error.message);
+    } else if (response.statusCode !== 200) {
+      console.error('Error:', `HTTP Status Code: ${response.statusCode}`);
+    } else {
+      try {
+        const filmsData = JSON.parse(body).results;
+        console.log(filmsData); // Add this line for debugging
+  
+        const numberOfFilms = filmsData.reduce((count, film) => {
+          if (film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)) {
+            return count + 1;
+          }
+          return count;
+        }, 0);
+        console.log(numberOfFilms);
+      } catch (parseError) {
+        console.error('Error:', 'Failed to parse response data');
+      }
     }
-  }
-});
+  });
