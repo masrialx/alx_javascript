e5b2895932e81c325c46535d0cfe4562fb738b55
Masri Alemu <masrialemu@gmail.com>
2023-09-11 20:00:50 +0300

    v3.2

--- javascript-network/3-request_store.js
+++ javascript-network/3-request_store.js
@@ -1,25 +1,32 @@
 const request = require('request');
 const fs = require('fs');
 
-if (process.argv.length !== 4) {
-  console.error('Usage: node 3-request_store.js <URL> <file path>');
+const url = process.argv[2]; // Get the URL from the command line argument
+const filePath = process.argv[3]; // Get the file path from the command line argument
+
+if (!url || !filePath) {
+  console.error('Please provide both the URL and file path as arguments.');
   process.exit(1);
 }
 
-const url = process.argv[2];
-const filePath = process.argv[3];
-
-request.get(url, (error, response, body) => {
+// Send a GET request to the specified URL
+request(url, (error, response, body) => {
   if (error) {
-    console.error('Error:', error.message);
-  } else if (response.statusCode !== 200) {
-    console.error('Error:', `HTTP Status Code: ${response.statusCode}`);
-  } else {
-    if (body.trim().length === 0) {
-      console.log(`Empty response from URL '${url}'.`);
-    } else {
-      fs.writeFileSync(filePath, body, 'utf-8');
-      console.log(`Contents of the URL '${url}' have been saved to '${filePath}'.`);
-    }
+    console.error('Error:', error);
+    process.exit(1);
   }
+
+  if (response.statusCode !== 200) {
+    console.error('Request failed with status code:', response.statusCode);
+    process.exit(1);
+  }
+
+  // Write the response body to the specified file (UTF-8 encoded)
+  fs.writeFile(filePath, body, 'utf-8', (err) => {
+    if (err) {
+      console.error('Error writing to file:', err);
+      process.exit(1);
+    }
+    console.log(`Successfully saved the content to ${filePath}`);
+  });
 });
