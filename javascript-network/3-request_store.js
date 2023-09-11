5c742b675befc375d9744110148243b1703dbef8
Masri Alemu <masrialemu@gmail.com>
2023-09-11 19:42:13 +0300

    v3.0

---
+++ javascript-network/3-request_store.js
@@ -0,0 +1,21 @@
+const request = require('request');
+const fs = require('fs');
+
+if (process.argv.length !== 4) {
+  console.error('Usage: node 3-request_store.js <URL> <file path>');
+  process.exit(1);
+}
+
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error.message);
+  } else if (response.statusCode !== 200) {
+    console.error('Error:', `HTTP Status Code: ${response.statusCode}`);
+  } else {
+    fs.writeFileSync(filePath, body, 'utf-8');
+    console.log(`Contents of the URL '${url}' have been saved to '${filePath}'.`);
+  }
+});
