be0a395cf3580136a5b26ec3ec97636252ee85f5
Masri Alemu <masrialemu@gmail.com>
2023-09-11 19:32:01 +0300

    v2.0

---
+++ javascript-network/2-starwars_count.js
@@ -0,0 +1,27 @@
+const request = require('request');
+
+if (process.argv.length !== 3) {
+  console.error('Usage: node 2-starwars_count.js <API URL>');
+  process.exit(1);
+}
+
+const apiUrl = process.argv[2];
+const characterId = 18;
+
+request.get(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error.message);
+  } else if (response.statusCode !== 200) {
+    console.error('Error:', `HTTP Status Code: ${response.statusCode}`);
+  } else {
+    try {
+      const filmsData = JSON.parse(body).results;
+      const numberOfFilms = filmsData.filter((film) =>
+        film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)
+      ).length;
+      console.log(numberOfFilms);
+    } catch (parseError) {
+      console.error('Error:', 'Failed to parse response data');
+    }
+  }
+});
