45efe4cdcdec624b245578aed3d0f84fde411e2f
Masri Alemu <masrialemu@gmail.com>
2023-09-21 20:41:28 +0300

    v7.1

--- Cookies_local_storage/src/index.js
+++ Cookies_local_storage/src/index.js
@@ -1,17 +1,13 @@
-// Check if Session Storage is supported
 if (typeof(Storage) === "undefined" || typeof(sessionStorage) === "undefined") {
   alert("Sorry, your browser does not support Session storage. Try again with a better one.");
 } else {
-  // Define available items
   const availableItems = ["Shampoo", "Soap", "Sponge", "Water"];
 
-  // Create a function to add an item to the cart in Session Storage
   function addItemToCart(item) {
       sessionStorage.setItem(item, "true");
       displayCart();
   }
 
-  // Create a function to build the store and display available items
   function createStore() {
       const ul = document.createElement("ul");
       for (const item of availableItems) {
@@ -25,7 +21,6 @@ if (typeof(Storage) === "undefined" || typeof(sessionStorage) === "undefined") {
       document.body.appendChild(ul);
   }
 
-  // Create a function to display the cart contents
   function displayCart() {
       const cartItems = Object.keys(sessionStorage);
       if (cartItems.length > 0) {
@@ -36,7 +31,6 @@ if (typeof(Storage) === "undefined" || typeof(sessionStorage) === "undefined") {
       }
   }
 
-  // Initialize the application by creating the store and displaying the cart
   createStore();
   displayCart();
 }
